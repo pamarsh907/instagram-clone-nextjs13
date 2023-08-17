2a0e011d1dce17b60cdb4f2db6801b45e91ff756
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-08-17 14:08:07 -0800

    added menu buttons to header

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon, PlusCircleIcon } from '@heroicons/react/24/outline'
+import { HomeIcon } from '@heroicons/react/24/solid'
 
 
 
 export default function Header() {
   return (
 
-        <div className='flex items-center justify-between max-w-6xl'>
+        <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
             <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
                 <Image
                     src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
@@ -29,7 +30,11 @@ export default function Header() {
                 <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
             </div>
 
-            <h1>Right side</h1>
+            <div className='flex space-x-4 items-center'>
+                <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+                <img src="https://yt3.googleusercontent.com/bkh2hC8aY-IQfQhykosVfnWZsUXK0dgxBaqJst7t5fbBO8qZWGy31h98l0qNuR4qWU59csDW8w=s176-c-k-c0x00ffffff-no-rj" alt="user-image" className='h-10 rounded-full cursor-pointer'/>
+            </div>
         </div>
 
 
