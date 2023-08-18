423fadde986a8fdb5a54281bf8960fdb986533ac
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-08-18 15:11:36 -0800

    added header and image to post

--- src/app/components/post.jsx
+++ src/app/components/post.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {
+    EllipsisHorizontalIcon,
+    HeartIcon,
+    ChartBarIcon,
+    BookmarkIcon,
+  } from "@heroicons/react/24/outline";
 
 export default function Post({img, userImg, caption, username, id}) {
   return (
-    <div>
-        <h1>{username}</h1>
+    <div className='bg-white my-7 border rounded-md'>
+       {/* post header  */}
+
+       <div className='flex items-center p-5'>
+            <img className="h-12 rounded-full object-cover border p-1 mr-3" src={userImg} alt={username} />
+            <p className='font-bold flex-1'>{username}</p>
+            <EllipsisHorizontalIcon className='h-5'/>
+       </div>
+
+       <img className='object-cover w-full' src={img} alt="" />
     </div>
   )
 }
