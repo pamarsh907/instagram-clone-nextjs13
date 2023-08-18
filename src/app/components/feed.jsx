5c93e10beb66cfd16c1bb9ece00d21ec47c5355d
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-08-18 15:30:02 -0800

    made feed section responsive

--- src/app/components/feed.jsx
+++ src/app/components/feed.jsx
@@ -4,8 +4,8 @@ import Posts from './posts'
 
 export default function Feed() {
   return (
-    <main>
-        <section>
+    <main className='grid grid-cols-1 md:grid-cols-3'>
+        <section className='md:col-span-2 md:max-w-6xl mx-auto'>
             {/* stories */}
             <Stories/>
 
@@ -14,7 +14,7 @@ export default function Feed() {
 
         </section>
 
-        <section>
+        <section className='hidden md:col-span-1 md:inline-grid'>
             {/* mini profile */}
 
             {/* suggestions */}
