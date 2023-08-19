077cb8d0894081b7ce8234f5b04cfb5e7ec4f67a
pamarsh907 <peter_aaron_marsh@hotmail.com>
2023-08-18 16:37:46 -0800

    added suggestions to feed

--- src/app/components/feed.jsx
+++ src/app/components/feed.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Stories from './stories'
 import Posts from './posts'
 import MiniProfile from './mini-profile'
+import Suggestions from './suggestions'
 
 export default function Feed() {
   return (
@@ -22,6 +23,7 @@ export default function Feed() {
 
 
                 {/* suggestions */}
+                <Suggestions/>
             </div>
         </section>
     </main>
